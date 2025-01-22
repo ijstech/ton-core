c67974b98049afbd5557b11d63c6ee1257771195
Celina-IJS <celina.dev@instajs.com>
2025-01-22 14:28:21 +0700

    update build

--- esbuild.bundle.js
+++ esbuild.bundle.js
@@ -1,38 +1,45 @@
-const dependencies = require('./package.json').dependencies || {};
-const packageName = require('./package.json').name;
+// const dependencies = require('./package.json').dependencies || {}
+// const packageName = require('./package.json').name
 
-const Fs = require('fs');
+const Fs = require('fs')
 
 async function readFile(fileName) {
   return new Promise((resolve, reject) => {
     Fs.readFile(fileName, 'utf8', function (err, data) {
-      if (err)
-        reject(err)
-      else
-        resolve(data)
+      if (err) reject(err)
+      else resolve(data)
     })
   })
 }
 
 async function build() {
-  let result = await require('esbuild').build({
-    entryPoints: ['src/index.ts'],
-    outdir: 'lib',
-    bundle: true,
-    minify: true,
-    format: 'cjs',
-    external: [
-      ...Object.keys(dependencies)
-    ],
-    plugins: [],
-    inject: ['./buffer-shim.js'],
-  }).catch(() => process.exit(1));
-  let content = await readFile('lib/index.js');
+  let result = await require('esbuild')
+    .build({
+      entryPoints: ['src/index.ts'],
+      outdir: 'lib',
+      bundle: true,
+      minify: true,
+      format: 'iife',
+      // external: [...Object.keys(dependencies)],
+      globalName: 'TonCore',
+      plugins: [],
+      target: 'esnext',
+      define: {
+        global: 'window'
+      },
+      inject: ['./buffer-shim.js'],
+    })
+    .catch(() => process.exit(1));
+
+  let content = await readFile('lib/index.js')
   content = `
-define("ton-core", (require,exports)=>{
+define("@scom/ton-core", ["require", "exports"], function (require, exports) {
+  Object.defineProperty(exports, "__esModule", { value: true }); 
   ${content}
-});
-`;
-  Fs.writeFileSync('lib/index.js', content);  
-};
-build();
+  if (typeof module !== "undefined" && module.exports) { module.exports = TonCore; } 
+    // exports.default = TonCore;
+    Object.assign(exports, TonCore)
+});`
+  Fs.writeFileSync('lib/index.js', content)
+}
+build()
